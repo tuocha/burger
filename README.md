208d322f31687569e3e3e0d430c55ebafe20ce89
tuocha <kippon47@gmail.com>
2019-08-25 01:30:02 -0400

    initialized readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # burger
+
+Link to the gitub repo: 
+https://tuocha.github.io/burger/
+
+Link to the Heroku deployment: 
+https://guarded-harbor-37700.herokuapp.com/
--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-var exphbs = require("express-handlebars");
 var express = require("express");
-var mysql = require("mysql");
-
 var app = express();
-
 
 var PORT = process.env.PORT || 8080;
 
@@ -12,6 +8,8 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ 
     defaultLayout: "main",

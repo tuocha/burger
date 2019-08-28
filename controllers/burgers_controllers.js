798b3f13a0c05274d3f8c61cf81e1aa579abdf8a
tuocha <kippon47@gmail.com>
2019-08-28 01:02:19 -0400

    added a little style

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -27,7 +27,7 @@ router.put("/api/burgers/:id", function (req, res) {
     console.log("condition", condition);
 
     burgers.updateOne({
-        devoured: 1
+        devoured: 0
     }, condition, function (result) {
         if (result.changedRows == 0) {
             // If no rows were changed, then the ID must not exist, so 404

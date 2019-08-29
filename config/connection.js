3f4bfaaf83fa654f833af1455b736d2fe0a6c029
tuocha <kippon47@gmail.com>
2019-08-29 01:03:26 -0400

    moved passwords to dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,14 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Fusrodah1!",
+  host: process.env.db_host,
+  port: process.env.db_port,
+  user: process.env.db_user,
+  password: process.env.db_password,
   database: "burgers_db"
 });
 }
